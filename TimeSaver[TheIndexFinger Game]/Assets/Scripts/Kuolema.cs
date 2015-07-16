1265ce466e2e6a04f813be416c6a2504d7f975f3
unknown <ojuho001@MDMA325W03.ksamk.ad>
2015-07-16 11:24:27 +0300

    Christ

---
+++ TimeSaver[TheIndexFinger Game]/Assets/Scripts/Kuolema.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Kuolema : MonoBehaviour {
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if (collider.tag == "Player") {
+			//Liike.dead = true;
+			//Liike.deathcooldown = 0.5f;
+			KeyboardMove.dead = true;
+		}
+	}
+}
